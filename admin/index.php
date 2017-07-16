77db7d9dfb5ba2d64311bbfd7db8d45d7a2b1589
jProgr <8537691@gmail.com>
2017-07-16 17:05:56 -0500

    Add admin panel

--- admin/index.php
+++ admin/index.php
@@ -1,12 +1,5 @@
 <!DOCTYPE html>
-<?php
-    include_once 'config.php';
 
-    $query = $pdo->prepare('SELECT * FROM blog_posts ORDER BY id DESC');
-    $query->execute();
-
-    $blogPosts = $query->fetchAll(PDO::FETCH_ASSOC);
-?>
 <html>
 <head>
     <meta charset="utf-8">
@@ -25,21 +18,7 @@
 
         <div class="row">
             <div class="col-md-8">
-
-                <!-- Prints blog posts obtained from database -->
-                <?php foreach ($blogPosts as $blogPost): ?>
-                    <div class="blog-post">
-                        <h2><?=$blogPost['title']?></h2>
-                        <p><a href="">by Alex</a> on XX/XX/XXXX</p>
-                        <div class="blog-post-image">
-                            <img src="images/placeholder_img.jpg" alt="">
-                        </div>
-                        <div class="blog-post-content">
-                            <?=$blogPost['content']?>
-                        </div>
-                    </div>
-                <?php endforeach ?>
-
+                <ul><li><a href="posts.php">Manage blog</a></li></ul>
             </div>
             <div class="col-md-4">
                 Jackie Chan DC Racing, formerly known as DC Racing, is a racing team that currently competes in the FIA World Endurance Championship and Asian Le Mans Series. The team is co-owned by Asian Le Mans champion David Cheng and actor Jackie Chan. Partnering with Jota Sport in WEC, the team fields two Oreca 07s: the No. 37 for Cheng, Alex Brundle and Tristan Gommendy, and the No. 38 for Ho-Pin Tung, Thomas Laurent and Oliver Jarvis.
--- admin/posts.php
+++ admin/posts.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <?php
-    include_once 'config.php';
+    include_once '../config.php';
 
     $query = $pdo->prepare('SELECT * FROM blog_posts ORDER BY id DESC');
     $query->execute();
@@ -12,7 +12,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <title>Blog</title>
+    <title>Admin</title>
 </head>
 <body>
     <div class="container">
@@ -24,22 +24,26 @@
         </div>
 
         <div class="row">
+            <h2>Posts</h2>
+            
             <div class="col-md-8">
+                <table class="table">
+                    <tr>
+                        <th>Title</th>
+                        <th>Edit</th>
+                        <th>Delete</th>
+                    </tr>
 
-                <!-- Prints blog posts obtained from database -->
-                <?php foreach ($blogPosts as $blogPost): ?>
-                    <div class="blog-post">
-                        <h2><?=$blogPost['title']?></h2>
-                        <p><a href="">by Alex</a> on XX/XX/XXXX</p>
-                        <div class="blog-post-image">
-                            <img src="images/placeholder_img.jpg" alt="">
-                        </div>
-                        <div class="blog-post-content">
-                            <?=$blogPost['content']?>
-                        </div>
-                    </div>
-                <?php endforeach ?>
-
+                    <!-- Prints list of blogposts from DB -->
+                    <?php foreach ($blogPosts as $blogPost): ?>
+                        <tr>
+                            <td><?=$blogPost['title']?></td>
+                            <td>Edit</td>
+                            <td>Delete</td>
+                        </tr>
+                    <?php endforeach ?>
+                    
+                </table>
             </div>
             <div class="col-md-4">
                 Jackie Chan DC Racing, formerly known as DC Racing, is a racing team that currently competes in the FIA World Endurance Championship and Asian Le Mans Series. The team is co-owned by Asian Le Mans champion David Cheng and actor Jackie Chan. Partnering with Jota Sport in WEC, the team fields two Oreca 07s: the No. 37 for Cheng, Alex Brundle and Tristan Gommendy, and the No. 38 for Ho-Pin Tung, Thomas Laurent and Oliver Jarvis.
