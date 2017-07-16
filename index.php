379e4751dbbafe080ea335dffac9397882039096
jProgr <8537691@gmail.com>
2017-07-16 13:25:40 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <title>Blog</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Title</h1>
+            </div>
+        </div>
+        <dov class="row">
+            <div class="col-md-8">
+                <div class="blog-post">
+                    <h2>Sample title</h2>
+                    <p><a href="">by Alex</a> on XX/XX/XXXX</p>
+                    <div class="blog-post-image">
+                        <img src="" alt="">
+                    </div>
+                    <div class="blog-post-content">
+                        The Los Alamos Laboratory, also known as Project Y, was a secret laboratory established by the Manhattan Project and operated by the University of California during World War II. Its mission was to design and build the first atomic bombs. Robert Oppenheimer was its first director, from 1943 to December 1945, when he was succeeded by Norris Bradbury. For scientists freely to discuss their work while preserving security, the laboratory was located in a remote part of New Mexico. The wartime laboratory occupied buildings that had once been part of the Los Alamos Ranch School.
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                Sidebar
+            </div>
+        </dov>
+    </div>
+</body>
+</html>
