d84d50928a9e2d982bd0b61c39275a4ad5719fa6
jProgr <8537691@gmail.com>
2017-07-30 11:21:21 -0500

    Fix indentation

--- app/models/BlogPost.php
+++ app/models/BlogPost.php
@@ -1,11 +1,12 @@
 <?php
 
-namespace App\Models;
+    namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+    use Illuminate\Database\Eloquent\Model;
 
-class BlogPost extends Model
-{
-    protected $table = 'blog_posts';
-    protected $fillable = ['title', 'content'];
-}
+    class BlogPost extends Model
+    {
+        protected $table = 'blog_posts';
+        protected $fillable = ['title', 'content'];
+    }
+?>
